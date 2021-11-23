f4929ebd18a90a41cce5ec344b4b5bc315171b47
Your Name <you@example.com>
2021-11-23 18:20:02 +0530

    this is cello class

---
+++ cello.java
@@ -0,0 +1,19 @@
+package com;
+
+public class cello
+{
+  public void fun()
+  {
+    int a=10;
+    int b=20;
+System.out.println("Before swapping a is:"+a);
+  System.out.println("Before swapping b is:"+b);
+     a=a+b;
+     b=a-b;
+     a=a-b;
+  System.out.println("After swapping a is:"+a);
+  System.out.println("After swapping b is:"+b);
+  }
+}
+
+
