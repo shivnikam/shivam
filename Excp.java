40ed67146fff36f390bb67c7d1813afabaf89776
Your Name <you@example.com>
2021-11-26 18:18:45 +0530

    this is exception handling file

---
+++ Excp.java
@@ -0,0 +1,19 @@
+class Excp
+{
+  public static void main(String args[])
+   {
+try
+{
+      int x=0;
+      int y=10;
+      
+      int m=y/x;
+   }
+catch(Exception e)
+{
+  System.out.println(e);
+}
+System.out.println("hello");
+
+}
+}
