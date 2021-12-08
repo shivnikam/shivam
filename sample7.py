503857b3a9833ff8f1ffe7f4c80d0e93d031a8a4
shivnikam <shivamnikam00744@gmail.com>
2021-12-08 23:12:00 +0530

    this is python sample code 7

---
+++ sample7.py
@@ -0,0 +1,14 @@
+my_name = 'shivam nikam'
+my_age =24
+my_height =74
+my_weight =180
+my_eyes = 'Blue'
+my_hair = 'Black'
+print(f"Let's talk about {my_name}.")
+print(f"I am {my_height} inches tall.")
+print(f"I am {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+ 
+total=my_age +my_height +my_weight
+print(f"If i add {my_age}, {my_height},{my_weight} I get{total}.")
