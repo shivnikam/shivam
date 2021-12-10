a17c76b01113d348a2b4e313f3be028e8b08cb68
shivnikam <shivamnikam00744@gmail.com>
2021-12-10 17:26:59 +0530

    this is set add elements in sets

---
+++ demo8.py
@@ -0,0 +1,9 @@
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"} # add items in sets
+
+thisset.add("orange")
+
+print(thisset)
+
