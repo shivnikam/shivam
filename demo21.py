5bf0c642fe234fc495e2dcd661fb498d86856dc5
shivnikam <shivamnikam00744@gmail.com>
2021-12-14 19:49:06 +0530

    index() and count() method in python

---
+++ demo21.py
@@ -0,0 +1,17 @@
+
+#index() return index position of that element
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+
+print(x)
+
+
+
+#count() return the number of times the value "cherry" appears in the  list
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.count("cherry")
+
+print(x)
