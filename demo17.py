4a9c4eb2dba0ffb536a078a9b86662da63639c7f
shivnikam <shivamnikam00744@gmail.com>
2021-12-14 19:44:47 +0530

    inheritance in python

---
+++ demo17.py
@@ -0,0 +1,17 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("Shivam", "Rahul")
+x.printname()   
+
+#Create a class Student, which will inherit the prop and methods from the Person class
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
