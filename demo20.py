24306ffede571ea87060b69accad2f217bd2518f
shivnikam <shivamnikam00744@gmail.com>
2021-12-14 19:48:20 +0530

    reverse and pop method in python

---
+++ demo20.py
@@ -0,0 +1,14 @@
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+
+
+print(fruits)
+
+#pop() is used to remove element from list
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.pop(1)
+
+print(fruits)
+
